c2b748f8e40e19c78ae258ab6dcdd043be23b740
AmirMostafa99 <am7870878@gmail.com>
2024-06-28 23:07:42 +0300

    updateAddTopic

--- DTOs/AddCollageDTO.cs
+++ DTOs/AddCollageDTO.cs
@@ -2,7 +2,7 @@
 {
     public class AddCollageDTO
     {
-        public int id { get; set; }
+        
         // [RegularExpression(@"^[\u0600-\u06FF]+$", ErrorMessage = "رجاء ادخال حروف عربيه")]
         public string collage_name { get; set; }
     }
